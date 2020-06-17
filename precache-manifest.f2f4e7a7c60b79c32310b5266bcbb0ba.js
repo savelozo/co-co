41fa629ff128f6f9b91570c9752720fe6d0ef07b
Seba Velozo <savelozo@uc.cl>
2020-06-17 01:06:54 -0400

    Updates

---
+++ precache-manifest.f2f4e7a7c60b79c32310b5266bcbb0ba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad041b2013f8b322bd362eb860356636",
+    "url": "/co-co/index.html"
+  },
+  {
+    "revision": "34151d37af8539518f77",
+    "url": "/co-co/static/css/main.bdc2ccb9.chunk.css"
+  },
+  {
+    "revision": "c03fc0da65db3dbd6318",
+    "url": "/co-co/static/js/2.5a030ed2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/co-co/static/js/2.5a030ed2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34151d37af8539518f77",
+    "url": "/co-co/static/js/main.35b7f7a2.chunk.js"
+  },
+  {
+    "revision": "11e77f9e9e54963b3b78",
+    "url": "/co-co/static/js/runtime-main.43db0bbf.js"
+  },
+  {
+    "revision": "37aa41f880be4666b7194c122d0d254c",
+    "url": "/co-co/static/media/LogoCOyCO.37aa41f8.png"
+  }
+]);
